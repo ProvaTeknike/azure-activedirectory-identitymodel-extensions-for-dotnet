a543c977c5f8a73a67d37f6740862a3320f268fa
Maria Furman <mafurman@microsoft.com>
2017-10-19 12:51:31 -0700

    Added in the internals visible to attribute

--- src/Microsoft.IdentityModel.Xml/KeyInfo.cs
+++ src/Microsoft.IdentityModel.Xml/KeyInfo.cs
@@ -28,9 +28,16 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.IdentityModel.Tokens;
+
+[assembly: InternalsVisibleTo("Microsoft.IdentityModel.Tokens.Saml, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67" +
+"871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0b" +
+"d333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307" +
+"e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c3" +
+"08055da9")]
 
 namespace Microsoft.IdentityModel.Xml
 {
@@ -143,7 +150,7 @@ namespace Microsoft.IdentityModel.Xml
         /// <summary>
         /// Returns true if the KeyInfo object can be matched with the specified SecurityKey, returns false otherwise.
         /// </summary>
-        public bool MatchKey(SecurityKey key)
+        internal bool MatchKey(SecurityKey key)
         {
             if (key is X509SecurityKey x509SecurityKey)
             {
